eb076a9c5537c9e88c7304a66244e962dac6abed
JamesSBeedle <jamessbeedle@gmail.com>
2021-02-02 17:02:06 +0000

    console work done

--- console.py
+++ console.py
@@ -9,11 +9,21 @@ artist_repository.delete_all()
 
 artist_1 = Artist("Mark")
 artist_repository.save(artist_1)
+
 album_1 = Album("How to make Friends and Influence People", "Rock", artist_1 )
 album_repository.save(album_1)
+
+album_1.genre = "Brit-Pop_Rock"
+album_repository.update(album_1)
 
 
 
 print(album_repository.select(album_1.id))
+
+for album in album_repository.select_all():
+    print(album.__dict__)
+
+
+pdb.set_trace()
 
 
